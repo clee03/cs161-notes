745717cb2e6d574d1933322c5a4c19580ba99f9a
Cathy Lee <cathylee03@gmail.com>
2016-06-28 15:11:18 -0600

    working on iterators

---
+++ WordFrequencyCounter.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+
+public class WordFrequencyCounter {
+  public WordFrequencyCounter {
+
+  }
+  public static void main (String [] args) {
+    System.out.println("hello, world");
+  }
+}
