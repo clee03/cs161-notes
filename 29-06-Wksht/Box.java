378c567015017e5e07ba978101d5165afb624569
Cathy Lee <cathylee03@gmail.com>
2016-06-29 22:02:01 -0600

    finish Box and Music practice

--- 29-06-Wksht/Box.java
+++ 29-06-Wksht/Box.java
@@ -4,39 +4,64 @@ public class Box {
     // use the area() method to assign the area instance variable
     // use the perimeter() method to assign the perimeter instance variable
     public Box (int length){
-
+      this.length = length;
+      this.area = this.area(length);
+      this.perimeter = this.perimeter(length);
 
 
 
     }
     // default length is 0
     public Box(){
-
+      this.length = 0;
 
 
 
     }
     public static int area(int length) {
-
+      return length * length;
 
     }
     public static int perimeter(int length) {
-
+      return length + length + length + length;
 
     }
     // format: length: 10, area: 100, parameter: 40
     public String toString() {
-
+      return String.format(">>Length: %d\nPerimeter: %d\nArea: %d\n",
+        this.length, this.perimeter, this.area);
 
 
     }
     public boolean equals (Object other){
-
+      if (other instanceof Box) {
+        Box b = (Box) other;
+        return this.length == b.length &&
+          this.perimeter == b.perimeter &&
+          this.area == b.area;
+      }
+      return false;
 
     }
     public int compareTo (Box b){
-
-
+      //arrange by len, per, area
+      int i = Integer.compare(this.length, b.length);
+      if (i != 0) {
+        return i;
+      } else {
+        i = Integer.compare(this.perimeter, b.perimeter);
+        if (i != 0) {
+          return i;
+        } else {
+          i = Integer.compare(this.area, b.area);
+          if (i != 0) {
+            return i;
+          } else {
+            return 0;
+          }
+        }
+      }
+      // return i;
 
 
     }
@@ -44,9 +69,9 @@ public class Box {
       System.out.println(">>start:");
 
       System.out.println(">>create p1 empty constructor");
-      Box p1 = new Box();
+      Box p1 = new Box(5);
       System.out.println(">>create p2 non-empty constructor");
-      Box p2 = new Box("Rick", "Ross");
+      Box p2 = new Box(0);
       System.out.println(">>create p2 = p3");
       Box p3 = p2;
 
--- 29-06-Wksht/Music.java
+++ 29-06-Wksht/Music.java
@@ -3,36 +3,96 @@ public class Music {
     private String title, artist, album;
     private int year;
     public Music (String title, String artist, String album, int year){
+      this.title = title;
+      this.artist = artist;
+      this.album = album;
+      this.year = year;
 
 
 
     }
     // by default assign the title to "Barbie Girl", artist to "Aqua", album to Aquarium, year to 1997
     public Music(){
-
+      this("Barbie Girl", "Aqua", "Aquarium", 1997);
 
 
 
     }
     // format: Title: Oops! I did it again, Artist: Britney Spears, Album: Oops! I did it again, Year: 2000
     public String toString() {
-
+      return String.format("Title: %s, Artist: %s, Album: %s, Year: %d",
+        this.title,
+        this.artist,
+        this.album,
+        this.year
+      );
 
 
     }
     public boolean equals (Object other){
-
-
+      if (other instanceof Music) {
+        Music m = (Music) other;
+        return this.title.equals(m.title) &&
+          this.artist.equals(m.artist) &&
+          this.album.equals(m.album) &&
+          this.year == m.year;
+      }
+      return false;
 
 
 
 
     }
     public int compareTo(Music m){
+      int i = this.title.compareTo(m.title);
+      if (i != 0) {
+        return i;
+      } else {
+        i = this.artist.compareTo(m.artist);
+        if (i != 0) {
+          return i;
+        } else {
+          i = this.album.compareTo(m.album);
+          if (i != 0) {
+            return i;
+          } else {
+            i = Integer.compare(this.year, m.year);
+            if (i != 0) {
+              return i;
+            } else {
+              return 0;
+            }
+          }
+        }
+      }
+    }
+
+    public static void main (String [] args){
+      System.out.println(">>start:");
+
+      System.out.println(">>create p1 empty constructor");
+      Music p1 = new Music("SOC", "NWA", "SOC", 1988);
+      System.out.println(">>create p2 non-empty constructor");
+      Music p2 = new Music();
+      System.out.println(">>create p2 = p3");
+      Music p3 = p2;
+
+      System.out.println(">>check toString p1");
+      System.out.println(p1.toString());
+      System.out.println(p2.toString());
+      System.out.println(p3.toString());
 
 
+      System.out.println(">>check equals p1 and p2: false");
+      System.out.println(p1.equals(p2));
 
+      System.out.println(">>check equals p2 and p3: true");
+      System.out.println(p2.equals(p3));
 
+      System.out.println(">>check compareTo p1 and p2: -1");
+      System.out.println(p1.compareTo(p2));
 
+      System.out.println(">>check compareTo p2 and p3: 0");
+      System.out.println(p2.compareTo(p3));
     }
 }
